5a1dfb0a7a22780eda5c33ff493a45b0a36cb890
Felipe Fardo <felipe.fardo@hotmail.com.br>
2025-11-14 01:33:43 -0300

    feat: create microservice app-invoices

---
+++ app-invoices/src/db/schema/invoices.ts
@@ -0,0 +1,10 @@
+import { timestamp } from "drizzle-orm/pg-core";
+import { text } from "drizzle-orm/pg-core";
+import { pgTable } from "drizzle-orm/pg-core";
+// import { customers } from "./customers.ts";
+
+export const invoices = pgTable("invoices",{
+  id: text().primaryKey(),
+  orderId: text().notNull(),
+  createdAt: timestamp().defaultNow().notNull()
+})
