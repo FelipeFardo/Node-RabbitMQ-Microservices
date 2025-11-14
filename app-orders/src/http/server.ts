a057ae44dd97bfff3189d6d46ee9b220411bb476
Felipe Fardo <felipe.fardo@hotmail.com.br>
2025-11-14 00:51:19 -0300

    chore: initial commit

---
+++ app-orders/src/http/server.ts
@@ -0,0 +1,37 @@
+import { fastify } from 'fastify'
+import { fastifyCors } from "@fastify/cors"
+import { z } from 'zod'
+import {
+  serializerCompiler,
+  validatorCompiler,
+  type ZodTypeProvider
+} from 'fastify-type-provider-zod'
+import { channels } from '../broker/channels/index.ts'
+
+const app = fastify().withTypeProvider<ZodTypeProvider>()
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.get("/health",()=>{
+  return 'OK'
+})
+
+app.post("/orders", {
+  schema: {
+    body: z.object({
+      amount: z.number(),
+    })
+  }
+}, async (request, reply)=>{
+  const {amount} = request.body
+  console.log('amount:', amount)
+
+  channels.orders.sendToQueue('orders', Buffer.from("Hello world"))
+
+  return reply.status(201).send()
+})
+
+app.listen({host:'0.0.0.0', port:3333}).then(()=>{
+  console.log("[Orders] HTTP Server running!")
+})
